834667d9a05aa457e6dd1657cfcf1c733b595623
Robert Heim <robert@robert-heim.de>
2025-12-22 02:28:31 +0100

    test: fix test compile error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:finanalyzer/app_router.dart';
+import 'package:finanalyzer/logging/logging_module.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +15,9 @@ import 'package:finanalyzer/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    final loggingModule = LoggingModule();
+    final log = loggingModule.logService.log;
+    await tester.pumpWidget(MyApp(loggingModule, AppRouter(log)));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
